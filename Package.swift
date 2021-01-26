2f368bfce814c34b6508eb343492735b56b04d1f
kkamakur <kkamakur@yahoo-corp.jp>
2021-01-26 15:31:04 +0900

    fix

--- Package.swift
+++ Package.swift
@@ -3,9 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "AppFeedback",
-    defaultLocalization: "en",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AppFeedback",
             targets: ["AppFeedback"]),
@@ -13,13 +11,7 @@ let package = Package(
     targets: [
         .target(
             name: "AppFeedback",
-            dependencies: [],
-            path: "AppFeedback"
-            ),
-        .testTarget(
-            name: "AppFeedbackTests",
-            dependencies: ["AppFeedback"],
-            path: "AppFeedbackTests"
-            ),
+            path: "./XCFramework/AppFeedback"
+            )
     ]
 )
